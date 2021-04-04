e77f7a48c14a642242313a483b307e6730fb09f1
Daniel de la Rosa <drdsdaniel@gmail.com>
2021-04-03 20:56:34 -0400

    ft_version usa nuevos nombres

--- R/ft_version.R
+++ R/ft_version.R
@@ -1,7 +1,12 @@
 ft_version <- function(tbl){
-  if("PERIALFA" %in% names(tbl)){
+  if ("tbl" %in% class(tbl)){
+    nombres <- dplyr::tbl_vars(tbl)
+  } else {
+    nombres <- names(tbl)
+  }
+  if(any(c("PERIALFA", "perialfa") %in% nombres)){
     1
-  } else if("EFT_PERIODO" %in% names(tbl)){
+  } else if("EFT_PERIODO" %in% nombres){
     2
   } else {
     stop("\n Ninguna de las variables PERIALFA o EFT_PERIODO fue encontrada en los datos.
